8a5ef55e0f1b48caa46c9ee0477da41914033a2b
Ruan <ruangustavo50711@gmail.com>
2020-11-16 16:19:16 -0300

    Desafio Finalizado

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Exercicio_C__3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("=======================================================");
+            Console.WriteLine("Bem vindo a piscina antes de entrar, Digite a sua idade");
+            Console.WriteLine("=======================================================");
+            int idade = int.Parse(Console.ReadLine());
+
+            if(idade >=5 && idade <=7) {
+                Console.WriteLine(" Você vai para a piscina infantil A");
+            }else{
+                if(idade >= 8 && idade <=10) {
+                    Console.WriteLine("Você vai pra piscina Infantil B");
+                }else {
+                    if(idade >=8 && idade <=10) {
+                        Console.WriteLine("Você vai pra piscina juvenil A");
+                    }else {
+                        if(idade >=11 && idade <= 13) {
+                            Console.WriteLine("Você Vai pra piscina Juvenil A");
+                        }else {
+                            if(idade >=14 && idade <= 17) {
+                                Console.WriteLine("Você vai pra piscina Juvenil B");
+                            }else {
+                                Console.WriteLine("Você vai nadar com os Seniores");
+                            }
+                        }
+                    }
+                }
+            }
+
+                
+            
+
+            
+
+
+
+
+
+
+
+
+        }   
+    }
+}
